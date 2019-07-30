68d8b0a42e5ca34ab31a08bad0faed68cff13efd
JDB02 <joshbrown02@hotmail.com>
2019-07-29 22:16:00 -0400

    Update email_phone-scraper.py

--- email_phone-scraper.py
+++ email_phone-scraper.py
@@ -2,12 +2,15 @@ import re
 from urllib.request import urlopen
 
 url = input("Enter a website to scan: (include http(s)://)\n")
-
 website = urlopen(url).read().decode('utf8')
 
 print()
-print("Phone Numbers:")
-print(re.findall(r"((?:\d{3}|\(\d{3}\))?(?:\s|-|\.)?\d{3}(?:\s|-|\.)\d{4})",website))
 print()
-print("Emails:")
-print(re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",website))
+print("Phone Numbers: ")
+numbers = (re.findall(r"((?:\d{3}|\(\d{3}\))?(?:\s|-|\.)?\d{3}(?:\s|-|\.)\d{4})",website))
+print (', '.join(map(str, numbers)))
+print()
+print()
+print("Emails: ")
+emails = (re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",website))
+print (', '.join(map(str, emails)))
